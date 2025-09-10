06b4c096206cd84a407945c47605084c79a0e304
Warren Manicane <manicane4321@gmail.com>
2025-09-11 04:43:17 +0800

    planning

--- main.py
+++ main.py
@@ -1,3 +1,73 @@
+# add grades to student
+# view grades of student
+# view student attendance (red = absent, green = present, orange = late, gray = no class)
+
+# recitation / additional grade tracker
+
+# other additionals
+# add student (first name, last name, middle name, age, student id no., student email, regular or irregular)
+# view student (alphabetically, regular or irregular)
+# view student grades (alphabetically, highest-lowest grade, lowest-highest grade)
+# view class average grade (see percentile / bar graph of the average)
+
+# ------------------------------------------------------------------------------------------
+# | Section / Course      | Edit                    | Settings       |
+# | BSCS 2-Y1-1 / ADET211 | Add Student             | Grading System |
+# | BSCS 2-Y1-2 / ADET211 | Remove Student          | 
+# | BSCS 2-Y1-3 / PLNG211 | Remove Section / Course |
+# | BSCS 2-Y1-4 / PLNG211 |  
+# | BSCS 2-Y1-5 / PLNG211 | 
+
+
+# ------------------------------------------------------------------------------------------
+# | Prelim | Midterm | Final |
+# ------------------------------------------------------------------------------------------
+# | * Attendance * | Recitation | Groupings | Grades | View GWA |
+# ------------------------------------------------------------------------------------------
+# | Students                    | Week 1 | Week 2 | Week 3 | Week 4 | Week 5 | Exam | 
+# | Caampued, John Kyle         |    /   |    -   |    -   |        |        |      |
+# | Manicane, John Warren D.    |    /   |    /   |    -   |        |        |      |
+# |                             |  Save  |  Edit  |  Edit  |  Edit  |  Edit  | Edit |
+# |                             | Cancel |        |        |        |        |      |
+
+
+# ------------------------------------------------------------------------------------------
+# | Prelim | Midterm | Final |
+# ------------------------------------------------------------------------------------------
+# | Attendance | * Recitation * | Groupings | Grades | View GWA |
+# ------------------------------------------------------------------------------------------
+# | Students                 | 1  | 2 | [+] [-]
+# | Caampued, John Kyle      |    |   |
+# | Manicane, John Warren D. | +5 |   |
+# | Marcelo, Jayvee          |    |   |
+# | Mirang, Joshua           |    |   |
+# |                                     [Edit]
+
+
+# ------------------------------------------------------------------------------------------
+# | Prelim | Midterm | Final | 
+# ------------------------------------------------------------------------------------------
+# | Attendance | Recitation | Groupings | Grades | View GWA |
+# ------------------------------------------------------------------------------------------
+# | Activities | Exams | [+] [-] |
+# ------------------------------------------------------------------------------------------
+# | Students                    | Act 1 | Act 2 | Act 3 | Act 4 | Act 5 | [+] [-]
+# | Caampued, John Kyle         | 90    |       |       |       |       |
+# | Manicane, John Warren D.    | 80    |       |       |       |       |
+# | Total                       | 100   | 100   | 120   | 120   | 120   |
+# |                             | Done  | Edit  | Edit  | Edit  | Edit  |
+# |                             | Cancel|       |       |       |       | 
+
+
+# ------------------------------------------------------------------------------------------
+# | Grading System                                               |
+# | Select Grading System Model  |
+# | Model              | Description
+# | Independent Term   | Final Grade = (Prelim × 30%) + (Midterm × 30%) + (Final × 40%)                                                   |
+# | Cumulative         | Midterm Grade = (Prelim × 50%) + (Midterm Work × 50%), Final Grade = (Midterm Grade × 50%) + (Final Work × 50%)  |
+# | Weighted Component | All Activities (quizzes, assignments, recitation) = 40%, Prelim Exam = 20%, Midterm Exam = 20%, Final Exam = 20% |
+
+
 def main():
     ...
 
