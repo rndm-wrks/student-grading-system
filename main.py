8e42e298dff4d9ca57d07f8196552ab3cb29f471
Warren Manicane <manicane4321@gmail.com>
2025-09-11 18:41:49 +0800

    idk

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-# add grades to student
-# view grades of student
 # view student attendance (red = absent, green = present, orange = late, gray = no class)
 
-# recitation / additional grade tracker
-
-# other additionals
 # add student (first name, last name, middle name, age, student id no., student email, regular or irregular)
 # view student (alphabetically, regular or irregular)
 # view student grades (alphabetically, highest-lowest grade, lowest-highest grade)
@@ -47,25 +42,57 @@
 # ------------------------------------------------------------------------------------------
 # | Prelim | Midterm | Final | 
 # ------------------------------------------------------------------------------------------
-# | Attendance | Recitation | Groupings | Grades | View GWA |
+# | Attendance | Recitation | Groupings | * Grades * | View GWA |
 # ------------------------------------------------------------------------------------------
 # | Activities | Exams | [+] [-] |
 # ------------------------------------------------------------------------------------------
 # | Students                    | Act 1 | Act 2 | Act 3 | Act 4 | Act 5 | [+] [-]
 # | Caampued, John Kyle         | 90    |       |       |       |       |
 # | Manicane, John Warren D.    | 80    |       |       |       |       |
-# | Total                       | 100   | 100   | 120   | 120   | 120   |
-# |                             | Done  | Edit  | Edit  | Edit  | Edit  |
-# |                             | Cancel|       |       |       |       | 
+
+# ------------------------------------------------------------------------------------------
+# | Info                        | Act 1 | Act 2 | Act 3 | Act 4 | Act 5 |
+# | Total                       | 100   | 120   | 120   | 120   | 120   |
+# | Average                     | 85    |       |       |       |       |
+# | Highest                     | 90    |       |       |       |       |
+# | Lowest                      | 80    |       |       |       |       |
 
 
 # ------------------------------------------------------------------------------------------
-# | Grading System                                               |
-# | Select Grading System Model  |
-# | Model              | Description
-# | Independent Term   | Final Grade = (Prelim × 30%) + (Midterm × 30%) + (Final × 40%)                                                   |
-# | Cumulative         | Midterm Grade = (Prelim × 50%) + (Midterm Work × 50%), Final Grade = (Midterm Grade × 50%) + (Final Work × 50%)  |
-# | Weighted Component | All Activities (quizzes, assignments, recitation) = 40%, Prelim Exam = 20%, Midterm Exam = 20%, Final Exam = 20% |
+# | Grading System - Independent Term                      |                                                                        | 
+# | Set Item Percentage        | Set Each Term Percentage  |
+# | Items         | Percentage | Term Items   | Percentage |
+# | Prelim  Grade | 30%        | Exam         | 60%        |
+# | Midterm Grade | 30%        | Activities   | 40%        |
+# | Final Grade   | 40%        |
+
+
+# ------------------------------------------------------------------------------------------
+# | Grading System - Cumulative                                                                                           | 
+# | Set Item Percentage                                         | Set Each Term Percentage |
+# | Items         | Calculations                                | Term Items | Percentage  | 
+# | Prelim Grade  | Prelim Grade                                | Activities | 60%         |
+# | Midterm Grade | (Prelim Grade x 50%) + (Midterm Items x 50) | Exams      | 40%         |
+# | Final Grade   | (Midterm Grade × 50%) + (Final Work × 50%)  |
+# | Overall Grade | Final Grade                                 |
+
+# ------------------------------------------------------------------------------------------
+# | Grading System - Weighted Component  |
+# | Set Term Percentage                  |
+# | Terms         | Percentage           |
+# | Prelim  Exam  | 20%                  |
+# | Midterm Exam  | 20%                  |
+# | Final Exam    | 20%                  |
+# | Activities    | 40%                  |
+
+# ------------------------------------------------------------------------------------------
+# | Grading System                                       |
+# | Set Term Percentage        | Set Item Percentage     |
+# | Terms         | Percentage | Items      | Percentage |
+# | Prelim Grade  | 30%        | Activities | 60%
+# | Midterm Grade | 30%        | Exams      | 40%
+# | Final Grade   | 40%        |    
+
 
 
 def main():
